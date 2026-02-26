fdf3935bdf554d63d65fc0677c5ecde2ee7095a5
mawadda alsubhi <mawadaeid22@gmail.com>
2026-02-26 04:01:21 +0300

    Frames : login, sign up, Dashboard

---
+++ src/gui/GUI.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gui;
+
+/**
+ *
+ * @author mawad
+ */
+public class GUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+           java.awt.EventQueue.invokeLater(() -> new Login().setVisible(true));
+    }
+}
